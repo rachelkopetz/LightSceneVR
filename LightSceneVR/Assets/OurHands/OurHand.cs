cd68f39bf5de46fa45bfeac6fd3b169f458be32d
rachelkopetz <116665116+rachelkopetz@users.noreply.github.com>
2023-01-18 12:41:49 -0800

    Right and left controller script

---
+++ LightSceneVR/Assets/OurHands/OurHand.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class OurHand : MonoBehaviour
+{
+    //public values to set in Unity, private used only in script
+    public GameObject ourHandPrefab;
+    public InputDeviceCharacteristics ourControllerCharacterisitcs;
+
+    private InputDevice ourDevice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitalizeOurHand();
+    }
+
+    void InitalizeOurHand()
+    {
+        // Check for our controllerscharacteristics
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(ourControllerCharacterisitcs, devices);
+
+        // If device identitfed, Instantiant a Hand
+        if (devices.Count > 0)
+        {
+            ourDevice = devices[0];
+
+            GameObject newHand = Instantiate(ourHandPrefab, transform);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Change animate position or re-initialize
+        if (ourDevice.isValid)
+        {
+            UpdateOurHand();
+        }
+        else
+        {
+            InitalizeOurHand();
+        }
+    }
+    void UpdateOurHand()
+    {
+        if (ourDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            Debug.Log("Trigger Value =" + triggerValue);
+        }
+        else
+        {
+            Debug.Log("Trigger not Active");
+        }
+        if (ourDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            Debug.Log("Grip Value =" + gripValue);
+        }
+        else
+        {
+            Debug.Log("Grip not Active");
+        }
+    }
+}
+
+
