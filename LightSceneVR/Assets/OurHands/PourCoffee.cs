28283a9b03289e9ce50c6958e0d5a190d829bf04
rachelkopetz <116665116+rachelkopetz@users.noreply.github.com>
2023-01-25 13:18:57 -0800

    pour coffee

---
+++ LightSceneVR/Assets/OurHands/PourCoffee.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourCoffee : MonoBehaviour
+{
+
+
+    public GameObject coffeeStream;
+    public Transform coffeePot;
+    public Transform coffeeLid;
+
+    private bool isPouring = false;
+    private bool pourCheck;
+    public float potTopHeight;
+    public float potMidHeight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        potMidHeight = coffeePot.position.y;
+        potTopHeight = coffeeLid.position.y;
+
+        if ((potTopHeight-potMidHeight) > 0)
+        {
+            pourCheck = true;
+        }
+        else
+        {
+            pourCheck = false;
+        }
+
+        if (isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+
+            if(isPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+    }
+
+    private void StartPour()
+    {
+        coffeeStream.SetActive(true);
+    }
+
+    private void EndPour()
+    {
+        coffeeStream.SetActive(false);
+    }
+}
