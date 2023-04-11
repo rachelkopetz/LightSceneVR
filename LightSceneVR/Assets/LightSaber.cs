de5fdbfc7d05578c6ecfee7e70a7c78f8cf96560
rachelkopetz <116665116+rachelkopetz@users.noreply.github.com>
2023-04-11 15:26:49 -0700

    lightSaber Animator

---
+++ LightSceneVR/Assets/LightSaber.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSaber : MonoBehaviour
+{
+   public Animator LightSaberAnimator;
+
+   public void TurnOnLightSaber()
+    {
+        LightSaberAnimator.SetTrigger("Turn LightSaber On");
+    }
+}
